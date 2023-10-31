5ea64493d7bf7e53c7932d0d57a04f52c30ee0a4
dh4k4d <anandandnagar@gmail.com>
2023-10-31 11:13:47 +0000

    Method get_current_date updated

--- Venv/driver.py
+++ Venv/driver.py
@@ -2,7 +2,7 @@ import logging, sys, os
 from logging.config import fileConfig
 import get_env_variables as gav
 from create_spark import get_spark_object
-# from validate import get_current_date
+from validate import get_current_date
 from file_format import get_file_format
 
 fileConfig('Properties/configuration/logging.config')
@@ -13,8 +13,8 @@ def main():
         logger.info('get_spark_object method calling...')
         spark = get_spark_object(gav.envn, gav.appName)
         logger.info("Spark object created - {}".format(spark))
-        #get_current_date(spark)
-        get_file_format(spark)
+        get_current_date(spark)
+        # get_file_format(spark)
 
     except Exception as e:
         logger.error("Error occured in main module - {}".format(str(e)))
--- Venv/validate.py
+++ Venv/validate.py
@@ -4,11 +4,11 @@ logger = logging.getLogger("Validate")
 
 def get_current_date(spark):
     try:
-        logger.info("get_current_date method started.")
+        logger.info("Method get_current_date started...")
         output = spark.sql("""SELECT CURRENT_DATE""")
-        logger.info("Got current date {}".format(output))
+        logger.info("Got current date {}".format(output.collect()))
     except Exception as e:
-        logger.error("{} - error occured in get_current_date.".format(str(e)))
+        logger.error("{} - error occured in get_current_date method.".format(str(e)))
         raise
     else:
         logger.info("Validate done in get_current_date.")
